0ba4164da28c56f5c10182ad913af1935a411a59
Erik Greif <bitfracture@gmail.com>
2018-03-01 17:53:00 -0800

    Fixed dependency for path for EB cli

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 cd ./condensation-forum
 echo "Installing AWS EB CLI..."
 pip3 install awsebcli --user
+export PATH="~/Library/Python/3.4/bin":$PATH
+export PATH="~/.local/bin":$PATH
 echo "Initializing EB. If prompted, choose region us-west-2..."
 eb init condensation-forum
 echo "Beginning deployment..."
