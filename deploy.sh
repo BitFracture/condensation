5f69fcacce5986b5a33c1286299ada4d1cefc3fc
Erik Greif <bitfracture@gmail.com>
2018-03-01 17:49:16 -0800

    Merged installer of AWS EB CLI

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd ./condensation-forum
+echo "Installing AWS EB CLI..."
+pip3 install awsebcli --user
 echo "Initializing EB. If prompted, choose region us-west-2..."
 eb init condensation-forum
 echo "Beginning deployment..."
