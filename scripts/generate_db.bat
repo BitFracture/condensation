5fbbdb0d2750b3ba39ae81c824f1306d2c003a09
Erik Greif <bitfracture@gmail.com>
2018-03-11 00:15:36 -0800

    Added python dependencies to windows regeneration script

--- scripts/generate_db.bat
+++ scripts/generate_db.bat
@@ -2,10 +2,10 @@
 pushd condensation-forum
 
 echo Generating Database
+pip install sqlalchemy --user
+pip install psycopg2 --user
 python3.exe _regenerate_db.py
 echo Finished
 
 popd
 @echo on
-
-
